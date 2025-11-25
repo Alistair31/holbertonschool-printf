6cc893821eda236f6d76441c3e910481f6b4b70a
Alistair31 <gabikill@hotmail.fr>
2025-11-25 10:47:19 +0100

    feat: update the library with basic structure

--- main.h
+++ main.h
@@ -1,6 +1,17 @@
 #ifndef main_h
 #define main_h
 
+#include <stdarg.h>
+/**
+ * struct allformat - choose the type
+ * @c: type of the string
+ * @f: pointer to function to call
+ */
 
+typedef struct allformat
+{
+	char c;
+	void (*f)(va_list args);
+} allformat;
 
 #endif
