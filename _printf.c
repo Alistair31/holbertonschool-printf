0d7cb2a24f749a0fac20796febbc52e2f0e73720
panaki-cyber <gillotpanaki@gmail.com>
2025-11-25 10:06:47 +0000

    Feat: Add a function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - print output according to a format.
+ * @format: list of types of arguments
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i = 0;
+}
