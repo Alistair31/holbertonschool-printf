6370c7d12e3f68cbbd0605957deb6a8f2d2d1561
panaki-cyber <gillotpanaki@gmail.com>
2025-11-26 22:52:56 +0000

    Fix: Add complete _printf implementation

--- _printf.c
+++ _printf.c
@@ -1,13 +1,77 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * _printf - print output according to a format.
- * @format: list of types of arguments
+ * handle_specifier - handles conversion specifiers
+ * @specifier: the conversion specifier character
+ * @args: va_list of arguments
+ *
+ * Return: number of characters printed
  */
+int handle_specifier(char specifier, va_list args)
+{
+	char c;
+	char *str;
+	int count = 0;
 
+	if (specifier == 'c')
+	{
+		c = va_arg(args, int);
+		count = _putchar(c);
+	}
+	else if (specifier == 's')
+	{
+		str = va_arg(args, char *);
+		count = print_string(str);
+	}
+	else if (specifier == '%')
+	{
+		count = _putchar('%');
+	}
+	else
+	{
+		count += _putchar('%');
+		count += _putchar(specifier);
+	}
+
+	return (count);
+}
+
+/**
+ * _printf - produces output according to a format
+ * @format: character string containing format specifiers
+ *
+ * Return: number of characters printed (excluding null byte)
+ */
 int _printf(const char *format, ...)
 {
-	va_list ap;
+	va_list args;
+	int count = 0;
 	int i = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				va_end(args);
+				return (-1);
+			}
+			count += handle_specifier(format[i], args);
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
+
+	va_end(args);
+	return (count);
 }
